267833ae2d7d18f920577f7daa803722ab3980fe
Pat Needham <patrick.h.needham@gmail.com>
2024-01-14 03:09:25 -0500

    layout adjustment; fixed test

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -12,8 +12,8 @@ export const meta: MetaFunction = () => [{ title: 'PPPAAATTT' }]
 export default function Index() {
 	return (
 		<main className="font-poppins grid h-full place-items-center">
-			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
-				<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
+			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-4">
+				<div className="mb-4 flex max-w-lg flex-col items-center text-left xl:order-2 xl:items-start">
 					<ContentLogo />
 					<ContentHeader />
 					<ContentBody />
