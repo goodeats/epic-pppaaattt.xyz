57e08901a447e50eaec0d4ba3d7e3aa48f782dbe
Pat Needham <patrick.h.needham@gmail.com>
2024-01-15 18:23:01 -0500

    copy change

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
 	return (
 		<main className="font-poppins grid h-full place-items-center">
 			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-4">
-				<div className="mb-4 flex max-w-lg flex-col items-center text-left xl:order-2 xl:items-start">
+				<div className="mb-4 flex max-w-lg flex-col items-center text-left xl:order-2 xl:mt-16 xl:items-start xl:self-start">
 					<ContentLogo />
 					<ContentHeader />
 					<ContentBody />
