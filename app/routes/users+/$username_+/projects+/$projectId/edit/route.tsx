0694dc1c00a84a37495ffbc52af5aa652be6e687
Pat Needham <patrick.h.needham@gmail.com>
2024-01-23 03:59:35 -0500

    meta for edit

--- app/routes/users+/$username_+/projects+/$projectId/edit/route.tsx
+++ app/routes/users+/$username_+/projects+/$projectId/edit/route.tsx
@@ -1,9 +1,10 @@
 import { invariantResponse } from '@epic-web/invariant'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { type MetaFunction, useLoaderData } from '@remix-run/react'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { requireUserId } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
+import { type loader as projectsLoader } from '../../route.tsx'
 import { EditProjectForm, action } from './edit-project-form.tsx'
 
 export { action }
@@ -30,6 +31,28 @@ export default function ProjectEdit() {
 	const data = useLoaderData<typeof loader>()
 
 	return <EditProjectForm project={data.project} />
+}
+
+export const meta: MetaFunction<
+	typeof loader,
+	{ 'routes/users+/$username_+/projects': typeof projectsLoader }
+> = ({ data, params, matches }) => {
+	const projectsMatch = matches.find(
+		m => m.id === 'routes/users+/$username_+/projects',
+	)
+	const displayName = projectsMatch?.data?.owner.name ?? params.username
+	const projectTitle = data?.project.name ?? 'Project'
+	const projectDescriptionSummary =
+		data && data.project.description.length > 100
+			? data?.project.description.slice(0, 97) + '...'
+			: 'No description'
+	return [
+		{ title: `Edit ${projectTitle} | ${displayName}'s Projects | XYZ` },
+		{
+			name: 'description',
+			content: projectDescriptionSummary,
+		},
+	]
 }
 
 export function ErrorBoundary() {
