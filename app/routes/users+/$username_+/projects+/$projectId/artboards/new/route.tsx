fb6cb3b7d1e179bced5fdb7a60ea148f7f95d068
Pat Needham <patrick.h.needham@gmail.com>
2024-01-26 00:56:31 -0500

    project artboard breadcrumbs

--- app/routes/users+/$username_+/projects+/$projectId/artboards/new/route.tsx
+++ app/routes/users+/$username_+/projects+/$projectId/artboards/new/route.tsx
@@ -1,8 +1,13 @@
 import { invariantResponse } from '@epic-web/invariant'
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { requireUserId } from '#app/utils/auth.server.ts'
+import { type BreadcrumbHandle } from '#app/utils/breadcrumbs.tsx'
 import { prisma } from '#app/utils/db.server.ts'
 import { NewArtboardForm, action } from './new-artboard-form.tsx'
+
+export const handle: BreadcrumbHandle = {
+	breadcrumb: () => 'New',
+}
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
--- app/routes/users+/$username_+/projects+/$projectId/artboards/route.tsx
+++ app/routes/users+/$username_+/projects+/$projectId/artboards/route.tsx
@@ -1,7 +1,12 @@
 import { invariantResponse } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { requireUserId } from '#app/utils/auth.server.ts'
+import { type BreadcrumbHandle } from '#app/utils/breadcrumbs'
 import { prisma } from '#app/utils/db.server.ts'
+
+export const handle: BreadcrumbHandle = {
+	breadcrumb: () => 'Artboards',
+}
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
