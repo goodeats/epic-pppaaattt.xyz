b97ce2129766a2c5ef1dffe243492f5047873d9c
Pat Needham <patrick.h.needham@gmail.com>
2024-04-15 19:07:31 -0400

    meta -- sketchy until I change it at the end

--- app/routes/sketch+/_index/route.tsx
+++ app/routes/sketch+/_index/route.tsx
@@ -1,9 +1,23 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import {
+	json,
+	type MetaFunction,
+	type LoaderFunctionArgs,
+} from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { DashboardBody, DashboardContent } from '#app/components/layout'
 import { getProjectsWithArtboards } from '#app/models/project/project.get.server'
 import { requireUserId } from '#app/utils/auth.server'
 import { Projects } from './components/projects'
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: 'Sketchy | XYZ' },
+		{
+			name: 'description',
+			content: 'Sketchy dashboard for XYZ',
+		},
+	]
+}
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request)
