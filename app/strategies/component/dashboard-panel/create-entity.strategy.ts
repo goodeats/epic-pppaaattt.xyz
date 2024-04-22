2c746f623c78a2401d3bfd6b64757f80788e3ca0
Pat Needham <patrick.h.needham@gmail.com>
2024-04-22 01:25:50 -0400

    added type

--- app/strategies/component/dashboard-panel/create-entity.strategy.ts
+++ app/strategies/component/dashboard-panel/create-entity.strategy.ts
@@ -1,10 +1,14 @@
-import { type NewDesignSchemaType } from '#app/schema/design'
+import {
+	DesignParentTypeIdEnum,
+	type designParentTypeIdEnum,
+	type NewDesignSchemaType,
+} from '#app/schema/design'
 import { NewArtboardVersionDesignSchema } from '#app/schema/design-artboard-version'
 import { Routes, type RoutePath } from '#app/utils/routes.utils'
 
 export interface IDashboardPanelCreateEntityStrategy {
 	route: RoutePath
-	parentTypeId: string
+	parentTypeId: designParentTypeIdEnum
 	formId: string
 	schema: NewDesignSchemaType // could also be layer
 	iconText: string
@@ -14,7 +18,8 @@ export class DashboardPanelCreateArtboardVersionDesignTypeStrategy
 	implements IDashboardPanelCreateEntityStrategy
 {
 	route: RoutePath = Routes.RESOURCES.API.V1.ARTBOARD_VERSION.DESIGN.CREATE
-	parentTypeId: string = 'artboardVersionId'
+	parentTypeId: designParentTypeIdEnum =
+		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
 	formId: string = 'artboard-version-design-create'
 	schema: NewDesignSchemaType = NewArtboardVersionDesignSchema
 	iconText = 'Add New Design'
