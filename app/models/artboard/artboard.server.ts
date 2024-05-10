fa0dcbf18cd7e47854840e3925f6abac4ddf260d
Pat Needham <patrick.h.needham@gmail.com>
2024-05-10 00:09:04 -0400

    replaced prisma queries from artboard.server

--- app/models/artboard/artboard.server.ts
+++ app/models/artboard/artboard.server.ts
@@ -1,11 +1,5 @@
 import { type Artboard } from '@prisma/client'
 import { type DateOrString } from '#app/definitions/prisma-helper'
-import {
-	type findArtboardArgsType,
-	type selectArgsType,
-	type whereArgsType,
-} from '#app/schema/artboard'
-import { prisma } from '#app/utils/db.server'
 import { type IArtboardBranchWithVersions } from '../artboard-branch/artboard-branch.server'
 import { type IDesignWithType } from '../design.server'
 import { type ILayerWithDesigns } from '../layer.server'
@@ -28,27 +22,4 @@ export interface IArtboardWithDesignsAndLayers extends IArtboard {
 }
 export interface IArtboardWithBranchesAndVersions extends IArtboard {
 	branches: IArtboardBranchWithVersions[]
-}
-
-export const findFirstArtboard = async ({
-	where,
-	select,
-}: findArtboardArgsType): Promise<Artboard | null> => {
-	return await prisma.artboard.findFirst({
-		where,
-		select,
-	})
-}
-
-export const findArtboardByIdAndOwner = async ({
-	id,
-	ownerId,
-	select,
-}: {
-	id: whereArgsType['id']
-	ownerId: whereArgsType['ownerId']
-	select?: selectArgsType
-}): Promise<Artboard | null> => {
-	const where = { id, ownerId }
-	return await findFirstArtboard({ where, select })
 }
