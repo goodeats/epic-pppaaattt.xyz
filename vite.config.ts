879c344c1b7e127f71cb47c989cf9289a6676fe2
Pat Needham <patrick.h.needham@gmail.com>
2024-05-15 14:45:18 -0400

    remix dev tools added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { vitePlugin as remix } from '@remix-run/dev'
+import { remixDevTools } from 'remix-development-tools/vite'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [
+		remixDevTools(),
 		remix({
 			ignoredRouteFiles: ['**/*'],
 			serverModuleFormat: 'esm',
+			future: { v3_fetcherPersist: true },
 			routes: async defineRoutes => {
 				return flatRoutes('routes', defineRoutes, {
 					ignoredRouteFiles: [
@@ -18,5 +22,6 @@ export default defineConfig({
 				})
 			},
 		}),
+		tsconfigPaths(),
 	],
 })
