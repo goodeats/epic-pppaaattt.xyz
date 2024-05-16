a8a8a107414be9388609c2d293b8bfbe76271d58
Pat Needham <patrick.h.needham@gmail.com>
2024-05-16 00:27:22 -0400

    can toggle selected artboard version layer from resource route

--- app/components/templates/panel/dashboard-entity-panel.actions.tsx
+++ app/components/templates/panel/dashboard-entity-panel.actions.tsx
@@ -12,8 +12,8 @@ import {
 import { type IDashboardPanelSelectEntityStrategy } from '#app/strategies/component/dashboard-panel/select-entity.strategy'
 import { type IDashboardPanelUpdateEntityVisibleStrategy } from '#app/strategies/component/dashboard-panel/update-entity-visible.strategy'
 import { SidebarPanelRowActionsContainer } from '..'
-import { FormFetcherIcon } from '../form/fetcher/icon'
 import { PanelEntityDeleteAction } from './dashboard-entity-panel.actions.delete'
+import { PanelEntityToggleSelectedAction } from './dashboard-entity-panel.actions.toggle-selected'
 import { PanelEntityToggleVisobleAction } from './dashboard-entity-panel.actions.toggle-visible'
 
 export const PanelEntityRowActions = ({
@@ -75,7 +75,7 @@ const PanelEntityAction = ({
 			)
 		case EntityActionType.SELECT:
 			return (
-				<PanelEntitySelectAction
+				<PanelEntityToggleSelectedAction
 					entity={entity as IEntitySelectable}
 					parent={parent}
 					strategyEntitySelect={
@@ -86,29 +86,4 @@ const PanelEntityAction = ({
 		default:
 			return null
 	}
-}
-
-export const PanelEntitySelectAction = ({
-	entity,
-	parent,
-	strategyEntitySelect,
-}: {
-	entity: IEntitySelectable
-	parent: IEntityParentType
-	strategyEntitySelect: IDashboardPanelSelectEntityStrategy
-}) => {
-	const { selected } = entity
-	return (
-		<FormFetcherIcon
-			entityId={entity.id}
-			parentId={parent.id}
-			parentTypeId={strategyEntitySelect.parentTypeId}
-			route={strategyEntitySelect.route}
-			formId={strategyEntitySelect.formId}
-			schema={strategyEntitySelect.schema}
-			icon={selected ? 'crosshair-2' : 'crosshair-1'}
-			iconText={strategyEntitySelect.iconText}
-			className={selected ? 'bg-secondary text-secondary-foreground' : ''}
-		/>
-	)
 }
