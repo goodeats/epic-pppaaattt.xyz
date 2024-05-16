1b7987a7e8dbf362e4768182f9cca85e96814dca
Pat Needham <patrick.h.needham@gmail.com>
2024-05-16 00:59:32 -0400

    removed unused properties from toggle selected enitity strategy

--- app/strategies/component/dashboard-panel/select-entity.strategy.ts
+++ app/strategies/component/dashboard-panel/select-entity.strategy.ts
@@ -1,27 +1,16 @@
-import { DesignParentTypeIdEnum } from '#app/schema/design'
 import {
-	type entityParentIdTypeEnum,
 	type entityActionTypeEnum,
 	EntityActionType,
-	type SelectEntitySchemaType,
 	type entityTypeEnum,
 	type entityParentTypeEnum,
 	EntityType,
 	EntityParentType,
 } from '#app/schema/entity'
-import { type SelectLayerSchemaType } from '#app/schema/layer'
-import { SelectArtboardVersionLayerSchema } from '#app/schema/layer-artboard-version'
-import { Routes, type RoutePath } from '#app/utils/routes.const'
 
 export interface IDashboardPanelSelectEntityStrategy {
 	actionType: entityActionTypeEnum
 	entityType: entityTypeEnum
 	parentType: entityParentTypeEnum
-	route: RoutePath
-	parentTypeId: entityParentIdTypeEnum
-	formId: string
-	schema: SelectEntitySchemaType
-	iconText: string
 }
 
 export class DashboardPanelSelectArtboardVersionLayerStrategy
@@ -30,11 +19,4 @@ export class DashboardPanelSelectArtboardVersionLayerStrategy
 	actionType: entityActionTypeEnum = EntityActionType.SELECT
 	entityType: entityTypeEnum = EntityType.LAYER
 	parentType: entityParentTypeEnum = EntityParentType.ARTBOARD_VERSION
-	route: RoutePath =
-		Routes.RESOURCES.API.V1.ARTBOARD_VERSION.LAYER.UPDATE.SELECTED
-	parentTypeId: entityParentIdTypeEnum =
-		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
-	formId: string = 'artboard-version-layer-select'
-	schema: SelectLayerSchemaType = SelectArtboardVersionLayerSchema
-	iconText = 'Select Layer'
 }
