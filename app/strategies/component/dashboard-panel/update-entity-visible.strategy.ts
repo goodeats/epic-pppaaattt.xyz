5c3f579e21ccaae20aff50b5dcfb331de5ecb962
Pat Needham <patrick.h.needham@gmail.com>
2024-05-16 00:58:35 -0400

    removed unused properties from toggle visible enitity strategy

--- app/strategies/component/dashboard-panel/update-entity-visible.strategy.ts
+++ app/strategies/component/dashboard-panel/update-entity-visible.strategy.ts
@@ -1,32 +1,16 @@
 import {
-	DesignParentTypeIdEnum,
-	type ToggleVisibleDesignSchemaType,
-} from '#app/schema/design'
-import { ToggleVisibleArtboardVersionDesignSchema } from '#app/schema/design-artboard-version'
-import { ToggleVisibleLayerDesignSchema } from '#app/schema/design-layer'
-import {
 	type entityActionTypeEnum,
-	type ToggleVisibleEntitySchemaType,
-	type entityParentIdTypeEnum,
 	EntityActionType,
 	type entityTypeEnum,
 	type entityParentTypeEnum,
 	EntityType,
 	EntityParentType,
 } from '#app/schema/entity'
-import { type ToggleVisibleLayerSchemaType } from '#app/schema/layer'
-import { ToggleVisibleArtboardVersionLayerSchema } from '#app/schema/layer-artboard-version'
-import { Routes, type RoutePath } from '#app/utils/routes.const'
 
 export interface IDashboardPanelUpdateEntityVisibleStrategy {
 	actionType: entityActionTypeEnum
 	entityType: entityTypeEnum
 	parentType: entityParentTypeEnum
-	route: RoutePath
-	parentTypeId: entityParentIdTypeEnum
-	formId: string
-	schema: ToggleVisibleEntitySchemaType
-	iconText: string
 }
 
 export class DashboardPanelUpdateArtboardVersionDesignVisibleStrategy
@@ -35,14 +19,6 @@ export class DashboardPanelUpdateArtboardVersionDesignVisibleStrategy
 	actionType: entityActionTypeEnum = EntityActionType.TOGGLE_VISIBLE
 	entityType: entityTypeEnum = EntityType.DESIGN
 	parentType: entityParentTypeEnum = EntityParentType.ARTBOARD_VERSION
-	route: RoutePath =
-		Routes.RESOURCES.API.V1.ARTBOARD_VERSION.DESIGN.UPDATE.VISIBLE
-	parentTypeId: entityParentIdTypeEnum =
-		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
-	formId: string = 'artboard-version-design-update-visible'
-	schema: ToggleVisibleDesignSchemaType =
-		ToggleVisibleArtboardVersionDesignSchema
-	iconText = 'Design'
 }
 
 export class DashboardPanelUpdateArtboardVersionLayerVisibleStrategy
@@ -51,13 +27,6 @@ export class DashboardPanelUpdateArtboardVersionLayerVisibleStrategy
 	actionType: entityActionTypeEnum = EntityActionType.TOGGLE_VISIBLE
 	entityType: entityTypeEnum = EntityType.LAYER
 	parentType: entityParentTypeEnum = EntityParentType.ARTBOARD_VERSION
-	route: RoutePath =
-		Routes.RESOURCES.API.V1.ARTBOARD_VERSION.LAYER.UPDATE.VISIBLE
-	parentTypeId: entityParentIdTypeEnum =
-		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
-	formId: string = 'artboard-version-layer-update-visible'
-	schema: ToggleVisibleLayerSchemaType = ToggleVisibleArtboardVersionLayerSchema
-	iconText = 'Design'
 }
 
 export class DashboardPanelUpdateLayerDesignVisibleStrategy
@@ -66,9 +35,4 @@ export class DashboardPanelUpdateLayerDesignVisibleStrategy
 	actionType: entityActionTypeEnum = EntityActionType.TOGGLE_VISIBLE
 	entityType: entityTypeEnum = EntityType.DESIGN
 	parentType: entityParentTypeEnum = EntityParentType.LAYER
-	route: RoutePath = Routes.RESOURCES.API.V1.LAYER.DESIGN.UPDATE.VISIBLE
-	parentTypeId: entityParentIdTypeEnum = DesignParentTypeIdEnum.LAYER_ID
-	formId: string = 'layer-design-update-visible'
-	schema: ToggleVisibleDesignSchemaType = ToggleVisibleLayerDesignSchema
-	iconText = 'Design'
 }
