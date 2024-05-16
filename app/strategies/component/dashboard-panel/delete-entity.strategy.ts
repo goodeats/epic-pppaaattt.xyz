c7440bea81eea29bd8a4f31347755b063f07704e
Pat Needham <patrick.h.needham@gmail.com>
2024-05-16 00:57:27 -0400

    removed unused properties from delete enitity strategy

--- app/strategies/component/dashboard-panel/delete-entity.strategy.ts
+++ app/strategies/component/dashboard-panel/delete-entity.strategy.ts
@@ -1,12 +1,4 @@
 import {
-	type DeleteDesignSchemaType,
-	DesignParentTypeIdEnum,
-} from '#app/schema/design'
-import { DeleteArtboardVersionDesignSchema } from '#app/schema/design-artboard-version'
-import { DeleteLayerDesignSchema } from '#app/schema/design-layer'
-import {
-	type entityParentIdTypeEnum,
-	type DeleteEntitySchemaType,
 	type entityActionTypeEnum,
 	EntityActionType,
 	type entityTypeEnum,
@@ -14,19 +6,10 @@ import {
 	EntityType,
 	EntityParentType,
 } from '#app/schema/entity'
-import { type DeleteLayerSchemaType } from '#app/schema/layer'
-import { DeleteArtboardVersionLayerSchema } from '#app/schema/layer-artboard-version'
-import { Routes, type RoutePath } from '#app/utils/routes.const'
-
 export interface IDashboardPanelDeleteEntityStrategy {
 	actionType: entityActionTypeEnum
 	entityType: entityTypeEnum
 	parentType: entityParentTypeEnum
-	route: RoutePath
-	parentTypeId: entityParentIdTypeEnum
-	formId: string
-	schema: DeleteEntitySchemaType
-	iconText: string
 }
 
 export class DashboardPanelDeleteArtboardVersionDesignStrategy
@@ -35,12 +18,6 @@ export class DashboardPanelDeleteArtboardVersionDesignStrategy
 	actionType: entityActionTypeEnum = EntityActionType.DELETE
 	entityType: entityTypeEnum = EntityType.DESIGN
 	parentType: entityParentTypeEnum = EntityParentType.ARTBOARD_VERSION
-	route: RoutePath = Routes.RESOURCES.API.V1.ARTBOARD_VERSION.DESIGN.DELETE
-	parentTypeId: entityParentIdTypeEnum =
-		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
-	formId: string = 'artboard-version-design-delete'
-	schema: DeleteDesignSchemaType = DeleteArtboardVersionDesignSchema
-	iconText = 'Delete Design'
 }
 
 export class DashboardPanelDeleteArtboardVersionLayerStrategy
@@ -49,12 +26,6 @@ export class DashboardPanelDeleteArtboardVersionLayerStrategy
 	actionType: entityActionTypeEnum = EntityActionType.DELETE
 	entityType: entityTypeEnum = EntityType.LAYER
 	parentType: entityParentTypeEnum = EntityParentType.ARTBOARD_VERSION
-	route: RoutePath = Routes.RESOURCES.API.V1.ARTBOARD_VERSION.LAYER.DELETE
-	parentTypeId: entityParentIdTypeEnum =
-		DesignParentTypeIdEnum.ARTBOARD_VERSION_ID
-	formId: string = 'artboard-version-layer-delete'
-	schema: DeleteLayerSchemaType = DeleteArtboardVersionLayerSchema
-	iconText = 'Delete Layer'
 }
 
 export class DashboardPanelDeleteLayerDesignStrategy
@@ -63,9 +34,4 @@ export class DashboardPanelDeleteLayerDesignStrategy
 	actionType: entityActionTypeEnum = EntityActionType.DELETE
 	entityType: entityTypeEnum = EntityType.DESIGN
 	parentType: entityParentTypeEnum = EntityParentType.LAYER
-	route: RoutePath = Routes.RESOURCES.API.V1.LAYER.DESIGN.DELETE
-	parentTypeId: entityParentIdTypeEnum = DesignParentTypeIdEnum.LAYER_ID
-	formId: string = 'layer-design-delete'
-	schema: DeleteDesignSchemaType = DeleteLayerDesignSchema
-	iconText = 'Delete Design'
 }
