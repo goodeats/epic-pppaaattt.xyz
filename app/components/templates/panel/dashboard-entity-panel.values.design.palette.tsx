2acfee004cab92f02a10e9bd174cbb597847e84b
Pat Needham <patrick.h.needham@gmail.com>
2024-05-16 04:50:01 -0400

    can edit design type palette from resource route

--- app/components/templates/panel/dashboard-entity-panel.values.design.palette.tsx
+++ app/components/templates/panel/dashboard-entity-panel.values.design.palette.tsx
@@ -1,26 +1,56 @@
+import { memo, useCallback } from 'react'
+import { SidebarPanelPopoverFormContainer } from '#app/components/layout/popover'
+import { type IDesignWithPalette } from '#app/models/design/design.server'
+import { DesignTypePaletteValue } from '#app/routes/resources+/api.v1+/design.type.palette.update.value'
 import { type IEntity } from '#app/schema/entity'
 import { SidebarPanelRowValuesContainer } from '..'
+import { PanelEntityPopoverAlt } from './dashboard-entity-panel.popover.alt'
+
+interface EntityProps {
+	entity: IEntity
+}
+
+const EntityPopover = memo(({ entity }: EntityProps) => {
+	const backgroundColor = (entity as IDesignWithPalette).palette.value
+
+	return (
+		<PanelEntityPopoverAlt name="Palette" backgroundColor={backgroundColor}>
+			<SidebarPanelPopoverFormContainer>
+				<span>Value</span>
+				<DesignTypePaletteValue
+					design={entity as IDesignWithPalette}
+					formLocation="popover"
+				/>
+			</SidebarPanelPopoverFormContainer>
+		</PanelEntityPopoverAlt>
+	)
+})
+EntityPopover.displayName = 'EntityPopover'
+
+const EntityMainForm = memo(({ entity }: EntityProps) => {
+	return <DesignTypePaletteValue design={entity as IDesignWithPalette} />
+})
+EntityMainForm.displayName = 'EntityMainForm'
 
 export const PanelEntityValuesDesignPalette = ({
 	entity,
 }: {
 	entity: IEntity
 }) => {
+	const entityPopover = useCallback(
+		() => <EntityPopover entity={entity} />,
+		[entity],
+	)
+
+	const entityMainForm = useCallback(
+		() => <EntityMainForm entity={entity} />,
+		[entity],
+	)
+
 	return (
 		<SidebarPanelRowValuesContainer>
-			Palette
-			{/* <PanelEntityPopover
-					name={type}
-					entity={entity}
-					strategyEntityValues={strategyEntityValues}
-				/>
-				<PanelEntityForm panelEntityForm={panelEntityMainForm} />
-				{panelEntityFormatIcon && (
-					<PanelEntityIcon panelEntityIcon={panelEntityFormatIcon} />
-				)}
-				{panelEntityBasisIcon && (
-					<PanelEntityIcon panelEntityIcon={panelEntityBasisIcon} />
-				)} */}
+			{entityPopover()}
+			{entityMainForm()}
 		</SidebarPanelRowValuesContainer>
 	)
 }
--- app/routes/resources+/api.v1+/design.type.palette.update.value.tsx
+++ app/routes/resources+/api.v1+/design.type.palette.update.value.tsx
@@ -1,24 +1,30 @@
-import {
-	type LoaderFunctionArgs,
-	json,
-	type DataFunctionArgs,
-} from '@remix-run/node'
+import { conform, useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
+import { json, type ActionFunctionArgs } from '@remix-run/node'
+import { useFetcher } from '@remix-run/react'
+import { useRef } from 'react'
+import { AuthenticityTokenInput } from 'remix-utils/csrf/react'
 import { redirectBack } from 'remix-utils/redirect-back'
+import { useHydrated } from 'remix-utils/use-hydrated'
+import { Input } from '#app/components/ui/input'
+import { type IDesignWithPalette } from '#app/models/design/design.server'
 import {
 	updateDesignTypePaletteValue,
 	validateDesignTypeUpdatePaletteValueSubmission,
 } from '#app/models/design-type/palette/palette.update.server'
+import { EntityParentIdType } from '#app/schema/entity'
 import { validateNoJS } from '#app/schema/form-data'
+import { EditDesignPaletteValueSchema } from '#app/schema/palette'
 import { requireUserId } from '#app/utils/auth.server'
+import { useDebounce, useIsPending } from '#app/utils/misc'
+import { Routes } from '#app/utils/routes.const'
 
 // https://www.epicweb.dev/full-stack-components
 
-export async function loader({ request }: LoaderFunctionArgs) {
-	await requireUserId(request)
-	return json({})
-}
+const route = Routes.RESOURCES.API.V1.DESIGN.TYPE.PALETTE.UPDATE.VALUE
+const schema = EditDesignPaletteValueSchema
 
-export async function action({ request }: DataFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
 	const userId = await requireUserId(request)
 	const formData = await request.formData()
 	const noJS = validateNoJS({ formData })
@@ -49,5 +55,94 @@ export async function action({ request }: DataFunctionArgs) {
 		{
 			status: status === 'error' || !updateSuccess ? 404 : 200,
 		},
+	)
+}
+
+export const DesignTypePaletteValue = ({
+	design,
+	formLocation = '',
+}: {
+	design: IDesignWithPalette
+	formLocation?: string
+}) => {
+	const designId = design.id
+	const paletteId = design.palette.id
+	const formId = `design-type-palette-update-value-${designId}-${paletteId}${
+		formLocation ? `-${formLocation}` : ''
+	}`
+
+	const fetcher = useFetcher<typeof action>()
+	const lastSubmission = fetcher.data?.submission
+	const isPending = useIsPending()
+	let isHydrated = useHydrated()
+	const [form, fields] = useForm({
+		id: formId,
+		constraint: getFieldsetConstraint(schema),
+		lastSubmission,
+		shouldValidate: 'onInput',
+		shouldRevalidate: 'onInput',
+		onValidate: ({ formData }) => {
+			// set hex chars to uppercase
+			const value = formData.get('value')
+			if (typeof value === 'string') {
+				formData.set('value', value.toUpperCase())
+			}
+			return parse(formData, { schema })
+		},
+		onSubmit: async (event, { formData }) => {
+			event.preventDefault()
+			fetcher.submit(formData, {
+				method: 'POST',
+				action: route,
+			})
+		},
+		defaultValue: {
+			value: design.palette.value || '',
+		},
+	})
+	const submitRef = useRef<HTMLButtonElement>(null)
+
+	const handleChangeSubmit = useDebounce((f: HTMLFormElement) => {
+		submitRef.current?.click()
+	}, 400)
+
+	// still do this until conform can change the value to uppercase
+	// or fetcher can handle it, like with theme
+	const handleInput = (input: HTMLInputElement) => {
+		input.value = input.value.toUpperCase()
+	}
+
+	return (
+		<fetcher.Form
+			method="POST"
+			action={route}
+			onChange={e => handleChangeSubmit(e.currentTarget)}
+			{...form.props}
+		>
+			<AuthenticityTokenInput />
+
+			<input type="hidden" name="no-js" value={String(!isHydrated)} />
+			<input type="hidden" name="id" value={paletteId} />
+			<input
+				type="hidden"
+				name={EntityParentIdType.DESIGN_ID}
+				value={designId}
+			/>
+
+			<Input
+				maxLength={6}
+				className="flex h-8"
+				onInput={e => handleInput(e.currentTarget)}
+				disabled={isPending}
+				{...conform.input(fields.value, {
+					ariaAttributes: true,
+				})}
+			/>
+
+			{/* form onChange click this to trigger useForm */}
+			<button type="submit" ref={submitRef} style={{ display: 'none' }}>
+				Submit
+			</button>
+		</fetcher.Form>
 	)
 }
