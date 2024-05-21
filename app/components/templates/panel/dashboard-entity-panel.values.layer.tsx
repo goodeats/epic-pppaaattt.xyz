b18a4084ca5c2b51105cd1360021dcfa2a287898
Pat Needham <patrick.h.needham@gmail.com>
2024-05-21 12:08:36 -0400

    can edit layer name from resource route

--- app/components/templates/panel/dashboard-entity-panel.values.layer.tsx
+++ app/components/templates/panel/dashboard-entity-panel.values.layer.tsx
@@ -1,22 +1,47 @@
+import { memo, useCallback } from 'react'
+import { SidebarPanelPopoverFormContainer } from '#app/components/layout/popover'
+import { type ILayer } from '#app/models/layer/layer.server'
+import { LayerName } from '#app/routes/resources+/api.v1+/layer.update.name'
 import { type IEntity } from '#app/schema/entity'
 import { SidebarPanelRowValuesContainer } from '..'
+import { PanelEntityPopoverAlt } from './dashboard-entity-panel.popover.alt'
+
+interface EntityProps {
+	entity: IEntity
+}
+
+const EntityPopover = memo(({ entity }: EntityProps) => {
+	return (
+		<PanelEntityPopoverAlt name="Template">
+			<SidebarPanelPopoverFormContainer>
+				<span>Name</span>
+				<LayerName layer={entity as ILayer} formLocation="popover" />
+			</SidebarPanelPopoverFormContainer>
+		</PanelEntityPopoverAlt>
+	)
+})
+EntityPopover.displayName = 'EntityPopover'
+
+const EntityMainForm = memo(({ entity }: EntityProps) => {
+	return <LayerName layer={entity as ILayer} />
+})
+EntityMainForm.displayName = 'EntityMainForm'
 
 export const PanelEntityValuesLayer = ({ entity }: { entity: IEntity }) => {
+	const entityPopover = useCallback(
+		() => <EntityPopover entity={entity} />,
+		[entity],
+	)
+
+	const entityMainForm = useCallback(
+		() => <EntityMainForm entity={entity} />,
+		[entity],
+	)
+
 	return (
 		<SidebarPanelRowValuesContainer>
-			Layer
-			{/* <PanelEntityPopover
-					name={type}
-					entity={entity}
-					strategyEntityValues={strategyEntityValues}
-				/>
-				<PanelEntityForm panelEntityForm={panelEntityMainForm} />
-				{panelEntityFormatIcon && (
-					<PanelEntityIcon panelEntityIcon={panelEntityFormatIcon} />
-				)}
-				{panelEntityBasisIcon && (
-					<PanelEntityIcon panelEntityIcon={panelEntityBasisIcon} />
-				)} */}
+			{entityPopover()}
+			{entityMainForm()}
 		</SidebarPanelRowValuesContainer>
 	)
 }
