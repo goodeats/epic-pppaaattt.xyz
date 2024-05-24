1519de9e1ac4357285734d59e31c3722a5573b4b
Pat Needham <patrick.h.needham@gmail.com>
2024-05-24 12:08:59 -0400

    wrap artwork canvas component in relative div so it fits in its container

--- app/components/templates/canvas/artwork-canvas.tsx
+++ app/components/templates/canvas/artwork-canvas.tsx
@@ -2,6 +2,10 @@ import { memo, useEffect, useRef } from 'react'
 import { type IArtworkVersionGenerator } from '#app/definitions/artwork-generator'
 import { canvasDrawService } from '#app/services/canvas/draw.service'
 
+// The ArtworkCanvas component is wrapped in React.memo to optimize performance by memoizing the component.
+// This prevents unnecessary re-renders when the props passed to the component have not changed.
+// Specifically, since this component involves canvas drawing operations which can be computationally expensive,
+// memoizing ensures that these operations are only re-executed when necessary, such as when the 'generator' prop changes.
 export const ArtworkCanvas = memo(
 	({ generator }: { generator: IArtworkVersionGenerator }) => {
 		const { width, height, background } = generator.settings
@@ -15,13 +19,16 @@ export const ArtworkCanvas = memo(
 		}, [canvasRef, generator])
 
 		return (
-			<canvas
-				id="canvas-editor"
-				ref={canvasRef}
-				width={width}
-				height={height}
-				style={{ backgroundColor: `#${background}` }}
-			/>
+			<div className="relative h-full w-full">
+				<canvas
+					id="canvas-editor"
+					ref={canvasRef}
+					width={width}
+					height={height}
+					style={{ backgroundColor: `#${background}` }}
+					className="h-full w-full"
+				/>
+			</div>
 		)
 	},
 )
