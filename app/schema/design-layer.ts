53a1749d12caf902140d801eda0a630a45510e90
Pat Needham <patrick.h.needham@gmail.com>
2024-05-28 22:34:08 -0400

    fixed using incorrect parsing function for new artwork versions which was parsing out design visible override

--- app/schema/design-layer.ts
+++ app/schema/design-layer.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod'
 import { DesignTypeEnum, type designTypeEnum } from './design'
 
-// copied from ./design.ts
-// may not be necessary?
 interface DesignLayer {
 	type: designTypeEnum
 	ownerId: string
