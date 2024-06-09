71afa4db987b93e5ad7d7cd05126e3722712a544
Pat Needham <patrick.h.needham@gmail.com>
2024-06-08 20:45:53 -0400

    updated remix-development-tools for Storage exceeded quota bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { glob } from 'glob'
-import { remixDevTools } from 'remix-development-tools/vite'
+import { remixDevTools } from 'remix-development-tools'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
